5e82ec3e7f29f0a82aaeec1a509d8ed057531bcd
Jens Reidel <jens@troet.org>
2020-06-02 21:36:16 +0200

    Simplify code

--- server.py
+++ server.py
@@ -6,23 +6,19 @@ async def handler(reader, writer):
     process = await asyncio.create_subprocess_shell("./stockfish", stdin=PIPE, stdout=PIPE, stderr=PIPE)
 
     tasks = {
-        asyncio.Task(process.stdout.readline()): 1,
-        asyncio.Task(reader.readline()): 2,
+        asyncio.Task(process.stdout.readline()): (process.stdout, writer),
+        asyncio.Task(reader.readline()): (reader, process.stdin),
     }
 
     while process.returncode is None or not process.stdout.at_eof():
         done, _ = await asyncio.wait(tasks, return_when=asyncio.FIRST_COMPLETED)
-        for fut in done:
-            val = tasks.pop(fut)
-            res = fut.result()
-            if val == 1:
-                writer.write(res)
-                await writer.drain()
-                tasks[asyncio.Task(process.stdout.readline())] = 1
-            else:
-                process.stdin.write(res)
-                await process.stdin.drain()
-                tasks[asyncio.Task(reader.readline())] = 2
+        fut = done.pop()
+        source, target = tasks.pop(fut)
+        res = fut.result()
+        target.write(res)
+        await target.drain()
+        tasks[asyncio.Task(source.readline())] = (source, target)
+
     writer.close()
 
 
